acfb66262941166dcf145d8fe5af10479d1a8573
tinaraissa88 <raissafabriciolima3@gmail.comm>
2024-08-06 23:01:49 -0300

    2 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import fundoimg from './assets/image 4.png'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <body class="bg-blue-950">
+        <h1 class="bg-current	">BAYMAX!</h1>
+        <img src="" alt="" />
+        <header class=''>
+          <a href="">HOME</a>
+          <a href="">EQUIPAMENTOS</a>
+          <a href="">SOBRE</a>
+          <a href="">PARTICIPANTES</a>
+        </header>
+        <img src={fundoimg} alt="img fundo" />
+        <p className='bg-blue-950'>Assim como no filme Big hero o personagem Baymax tem o objetivo de auxiliar na sua saúde. o robo vem com a intenção de tornar seu dia a dia mais pratico e funcional venha conhecer mais sobre.</p>
+      </body>
+
   )
 }
 
 export default App
+
